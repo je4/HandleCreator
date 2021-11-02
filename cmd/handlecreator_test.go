0759f67e0f27b6d3b07e06d5182089f1b38da92a
Jürgen Enge <je4@users.noreply.github.com>
2021-11-02 15:57:21 +0100

    test added

---
+++ cmd/handlecreator_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/je4/HandleCreator/v2/pkg/client"
+	"github.com/je4/HandleCreator/v2/pkg/server"
+	lm "github.com/je4/utils/v2/pkg/logger"
+	"github.com/phayes/freeport"
+	"net/url"
+	"os"
+	"testing"
+	"time"
+)
+
+var hcClient *client.HandleCreatorClient
+var replStmt *sqlmock.ExpectedPrepare
+var mock sqlmock.Sqlmock
+
+func TestMain(m *testing.M) {
+	logger, lf := lm.CreateLogger(
+		"HandleCreator",
+		"",
+		nil,
+		"DEBUG",
+		"%{time:2006-01-02T15:04:05.000} %{module}::%{shortfunc} [%{shortfile}] > %{level:.5s} - %{message}",
+	)
+	defer lf.Close()
+
+	// get database connection handle
+	var db *sql.DB
+	var err error
+	db, mock, err = sqlmock.New()
+	if err != nil {
+		logger.Panicf("error opening database: %v", err)
+	}
+	// close on shutdown
+	defer db.Close()
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		logger.Panicf("error pinging database: %v", err)
+	}
+
+	mock.ExpectPrepare("REPLACE INTO test.handles_handle\\(handle, data\\)").
+		ExpectExec().
+		WithArgs("0000.test", "https://test.org").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	port, err := freeport.GetFreePort()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	addr := fmt.Sprintf("localhost:%v", port)
+	srv, err := server.NewServer(addr, db, "test", logger, os.Stdout, "swordfish", []string{"HS512"})
+	if err != nil {
+		logger.Panicf("error initializing server: %v", err)
+	}
+	time.Sleep(2 * time.Second)
+
+	go func() {
+		if err := srv.ListenAndServe("", ""); err != nil {
+			logger.Fatalf("server died: %v", err)
+		}
+	}()
+	defer srv.Shutdown(context.Background())
+
+	hcClient, err = client.NewHandleCreatorClient(fmt.Sprintf("http://%s", addr), "swordfish", "HS512", false, logger)
+	if err != nil {
+		logger.Fatalf("cannot create handlecreatorclient: %v", err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestPing(t *testing.T) {
+	if hcClient == nil {
+		t.Error("hcClient is nil")
+		return
+	}
+	if err := hcClient.Ping(); err != nil {
+		t.Errorf("ping error: %v", err)
+	}
+}
+
+func TestCreate(t *testing.T) {
+	if hcClient == nil {
+		t.Error("hcClient is nil")
+		return
+	}
+
+	url, _ := url.Parse("https://test.org")
+	hcClient.Create("0000.test", url)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
---
+++ pkg/client/HandleCreatorClient.go
@@ -0,0 +1,95 @@
+package client
+
+import (
+	"bytes"
+	"crypto/sha512"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/je4/HandleCreator/v2/pkg/server"
+	"github.com/je4/utils/v2/pkg/JWTInterceptor"
+	"github.com/op/go-logging"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type HandleCreatorClient struct {
+	client http.Client
+	addr   string
+	logger *logging.Logger
+}
+
+func NewHandleCreatorClient(addr string, jwtKey, jwtAlg string, certSkipVerify bool, logger *logging.Logger) (*HandleCreatorClient, error) {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: certSkipVerify}
+	tr, err := JWTInterceptor.NewJWTTransport(nil, sha512.New(), "", jwtKey, jwtAlg, 30*time.Second)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot create jwt transport")
+	}
+
+	hsc := &HandleCreatorClient{
+		addr: addr,
+		client: http.Client{
+			Transport: tr,
+		},
+		logger: logger,
+	}
+	return hsc, nil
+}
+
+func (hsc *HandleCreatorClient) Ping() error {
+	u := fmt.Sprintf("%s/ping", hsc.addr)
+	resp, err := hsc.client.Get(u)
+	if err != nil {
+		hsc.logger.Errorf("cannot query %s: %v", u, err)
+		return errors.Wrapf(err, "cannot query %s", u)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New(fmt.Sprintf("error pinging HandleCreator Service: invalid status %s", resp.Status))
+	}
+	jdec := json.NewDecoder(resp.Body)
+	result := &server.ApiResult{}
+	if err := jdec.Decode(result); err != nil {
+		hsc.logger.Errorf("cannot read and decode result: %v", err)
+		return errors.Wrap(err, "cannot read and decode result")
+	}
+	if result.Status != "ok" {
+		return errors.New(fmt.Sprintf("error pinging HandleCreator Service: %s", result.Message))
+	}
+	return nil
+}
+
+func (hsc *HandleCreatorClient) Create(handle string, URL *url.URL) error {
+	createStruct := struct {
+		Handle string `json:"handle"`
+		Url    string `json:"url"`
+	}{
+		Handle: handle,
+		Url:    URL.String(),
+	}
+	data, err := json.Marshal(createStruct)
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("cannot marshal create struct %v", createStruct))
+	}
+	u := fmt.Sprintf("%s/create", hsc.addr)
+	hsc.logger.Infof("creating handle %s", handle)
+	resp, err := hsc.client.Post(u,
+		"application/json",
+		bytes.NewBuffer(data))
+	if err != nil {
+		hsc.logger.Errorf("cannot query %s: %v", u, err)
+		return errors.Wrapf(err, "cannot query %s", u)
+	}
+	result, err := io.ReadAll(resp.Body)
+	if err != nil {
+		hsc.logger.Errorf("cannot read result: %v", err)
+		return errors.Wrap(err, "cannot read result")
+	}
+	if resp.StatusCode != http.StatusCreated {
+		hsc.logger.Errorf("handle %s not created: %s", handle, string(result))
+		return errors.New(fmt.Sprintf("handle %s not created: %s", handle, string(result)))
+	}
+	return nil
+}
